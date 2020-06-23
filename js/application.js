fbf77dd217af59799677235648d22b479051ae93
Mohammed Hussen <mohamedhussen1994@gmail.com>
2020-06-23 18:16:52 +0200

    adding more features

--- js/application.js
+++ js/application.js
@@ -13,9 +13,17 @@
   });
  
   function drawChartJS() {
- 
+    
+    // getting the report title that the user enter in the config window
+    var report_title = tableau.extensions.settings.get("report_title");
+    // adding report tilte
+    $("#title").html(report_title);
+    console.log(report_title);
+    // getting the report height that the user enter in the config window
+    var report_height = tableau.extensions.settings.get("report_height");
+    // getting the worksheet name that the user choose from the config window
     var worksheetName = tableau.extensions.settings.get("worksheet");
- 
+    
     const worksheets=tableau.extensions.dashboardContent.dashboard.worksheets;
     var worksheet=worksheets.find(function (sheet) {
       return sheet.name===worksheetName;
@@ -27,13 +35,13 @@
       var worksheetcolumns = summdata.columns;
       var columnsNames = [];
 
-      console.log(worksheetcolumns);
+      //console.log(worksheetcolumns);
       // get the columns names
       for (var m=0; m<worksheetcolumns.length; m++){
         columnsNames.push(worksheetcolumns[m].fieldName);
       }
 
-      console.log(summdata);
+      //console.log(summdata);
       for (var i=0; i<worksheetData.length; i++) {
         var temp = {};
         for (var j=0; j<columnsNames.length; j++){
@@ -42,45 +50,77 @@
         }
         test_data.push(temp);
       }
-      console.log(test_data);
+      //console.log(test_data);
 
       //create Tabulator on DOM element with id "example-table"
       var table = new Tabulator("#example-table", {
-      height:300, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+      height: report_height, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
       data:test_data, //assign data to table
       //layout:"fitColumns", //fit columns to width of table (optional)
       autoColumns: true,
-      //resizableColumns:true, // this option takes a boolean value (default = true)
-      //movableColumns: true, //Allow users to move and reorder columns 
-      //movableRows: true, //Allow users to move and reorder rows.
-      //headerSort: true, //Enable or disable header sorting on all columns. (default = true)
+      resizableColumns: true, // this option takes a boolean value (default = true)
+      movableColumns: true, //Allow users to move and reorder columns 
+      movableRows: true, //Allow users to move and reorder rows.
+      headerSort: true, //Enable or disable header sorting on all columns. (default = true)
       //persistentLayout: false, //Enable persistsnt storage of column layout information (default = false)
-      //pagination:"local",
-      //paginationSizeSelector:[2,4,6,8], // you can set it to ture instead if you want to auto select the list elements
+      pagination:"local",
+      paginationSizeSelector:true, // you can set it to ture instead if you want to auto select the list elements
+      // using callback to handle styling on the table
+      renderComplete:function(){
+        var header_color = tableau.extensions.settings.get("header_color");
+        var header_font_color = tableau.extensions.settings.get("header_font_color");
+        var even_color = tableau.extensions.settings.get("even_color");
+        var even_font_color = tableau.extensions.settings.get("even_font_color");
+        var odd_color = tableau.extensions.settings.get("odd_color");
+        var odd_font_color = tableau.extensions.settings.get("odd_font_color");
+        
+        // change the background color of the header
+        var x = document.getElementsByClassName("tabulator-col");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.backgroundColor = header_color;
+          x[i].style.color = header_font_color;
+        };
+        // chaning the background color of the even rows
+        var x = document.getElementsByClassName("tabulator-row-even");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.backgroundColor = even_color;
+          x[i].style.color = even_font_color;
+        };
+        // changing the background color of odd rows
+        var x = document.getElementsByClassName("tabulator-row-odd");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].style.backgroundColor = odd_color;
+          x[i].style.color = odd_font_color;
+        };
+      },
     });
- 
+
+      //trigger download of data.csv file
+      document.getElementById("download-csv").addEventListener("click", function(){
+        table.download("csv", "data.csv");
+      });
+
+      //trigger download of data.xlsx file
+      document.getElementById("download-xlsx").addEventListener("click", function(){
+        table.download("xlsx", "data.xlsx", {sheetName:"My Data"});
+      });
+
+      //trigger download of data.pdf file
+      document.getElementById("download-pdf").addEventListener("click", function(){
+        table.download("pdf", "data.pdf", {
+            orientation:"portrait", //set page orientation to portrait
+            title:"Example Report", //add title to report
+        });
+      });
       
     });
+
+
   }
 
-  //trigger download of data.csv file
-  document.getElementById("download-csv").addEventListener("click", function(){
-    table.download("csv", "data.csv");
-  });
-
-
-  //trigger download of data.xlsx file
-  document.getElementById("download-xlsx").addEventListener("click", function(){
-    table.download("xlsx", "data.xlsx", {sheetName:"My Data"});
-  });
-
-  //trigger download of data.pdf file
-  document.getElementById("download-pdf").addEventListener("click", function(){
-    table.download("pdf", "data.pdf", {
-        orientation:"portrait", //set page orientation to portrait
-        title:"Example Report", //add title to report
-    });
-  });
  
   function configure() {
     const popupUrl=`${window.location.href}/dialog.html`;
--- js/dialog.js
+++ js/dialog.js
@@ -14,9 +14,26 @@
             $("#selectWorksheet").append("<option value='" + worksheet.name + "'>" + worksheet.name + "</option>");
         });
         var worksheetName = tableau.extensions.settings.get("worksheet");
+        var header_color = tableau.extensions.settings.get("header_color");
+        var header_font_color = tableau.extensions.settings.get("header_font_color");
+        var even_color = tableau.extensions.settings.get("even_color");
+        var even_font_color = tableau.extensions.settings.get("even_font_color");
+        var odd_color = tableau.extensions.settings.get("odd_color");
+        var odd_font_color = tableau.extensions.settings.get("odd_font_color");
+        var report_title = tableau.extensions.settings.get("report_title");
+        var report_height = tableau.extensions.settings.get("report_height");
         //$("#SelectChartType").val(tableau.extensions.settings.get("ChartType"));
         if (worksheetName != undefined) {
+            // if the worksheet name already exit save it the config page
             $("#selectWorksheet").val(worksheetName);
+            $("#header-color").val(header_color);
+            $("#header-font-color").val(header_font_color);
+            $("#even-color").val(even_color);
+            $("#even-font-color").val(even_font_color);
+            $("#odd-color").val(odd_color);
+            $("#odd-font-color").val(odd_font_color);
+            $("#report-title").val(report_title);
+            $("#report-height").val(report_height);
             columnsUpdate();
         }
  
@@ -43,9 +60,25 @@
     }
  
     function saveButton() {
- 
+        // saving the selected sheet name to the settings
         tableau.extensions.settings.set("worksheet", $("#selectWorksheet").val());
- 
+        // saving the header color value to the settings
+        tableau.extensions.settings.set("header_color", $("#header-color").val());
+        // saving the header font color value to the settings
+        tableau.extensions.settings.set("header_font_color", $("#header-font-color").val());
+        // saving the even rows color
+        tableau.extensions.settings.set("even_color", $("#even-color").val());
+        // saving the even font rows color
+        tableau.extensions.settings.set("even_font_color", $("#even-font-color").val());
+        // saving the odd rows color
+        tableau.extensions.settings.set("odd_color", $("#odd-color").val());
+        // saving the odd font rows color
+        tableau.extensions.settings.set("odd_font_color", $("#odd-font-color").val());
+        // saving report title
+        tableau.extensions.settings.set("report_title", $("#report-title").val());
+        // saving report height
+        tableau.extensions.settings.set("report_height", $("#report-height").val())
+        
         tableau.extensions.settings.saveAsync().then((currentSettings) => {
             tableau.extensions.ui.closeDialog("10");
         });
